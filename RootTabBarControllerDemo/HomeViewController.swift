1e71b295b122fb3c2f565d2c704443f44f6fceb2
Vic <iverson905117@gmail.com>
2020-04-18 19:13:35 +0800

    first commit

---
+++ RootTabBarControllerDemo/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  RootTabBarControllerDemo
+//
+//  Created by 康志斌 on 2020/4/18.
+//  Copyright © 2020 AppChihPin. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
